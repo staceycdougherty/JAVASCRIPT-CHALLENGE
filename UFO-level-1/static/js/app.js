8832619e959a80085d00c192538214f0689213fd
Stacey Dougherty <staceydougherty@Staceys-MacBook-Air.local>
2021-06-27 16:55:09 -0700

    created references

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,7 +4,7 @@ var tableData = data;
 // Creating References
 var tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
-var inputFieldDate = d3.select("#datetime");
+var inputDate = d3.select("#datetime");
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
 
