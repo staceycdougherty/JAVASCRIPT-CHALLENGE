981f9dbd05797da54ddfc85451bcc0e15802a2a6
Stacey Dougherty <staceydougherty@Staceys-MacBook-Air.local>
2021-06-27 18:10:54 -0700

    struggling with my filter

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,7 +5,6 @@ var tableData = data;
 var tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
 var inputDate = d3.select("#datetime");
-var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
 // add data to table on page
 data.forEach(tableData => {
@@ -16,3 +15,29 @@ data.forEach(tableData => {
     });
   });
   
+// listen-button
+
+// Create event handlers 
+button.on("click", runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  tbody.html("");
+
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select(inputDate);
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+
+  console.log(filteredData);
+};
